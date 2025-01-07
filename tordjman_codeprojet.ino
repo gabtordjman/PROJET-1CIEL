277b67cfbe4abf76107eba73094415ddf7f5680c
Gabriel TORDJMAN <122033995+gabtordjman@users.noreply.github.com>
2025-01-07 14:54:36 +0100

    Ajout d'éléments

--- tordjman_codeprojet.ino
+++ tordjman_codeprojet.ino
@@ -1,46 +1,50 @@
-// Projet 1CIEL - Wemos Weather 
-// Réalisé par Gabriel TORDJMAN - 2024-2025
+// Wemos Weather - 2024-2026 
+// Projet CIEL par Gabriel TORDJMAN BARRENQUI 
 
 #include <WiFiClient.h> //Pour la connexion a un réseau wifi défini ci-dessous
 #include <ESP8266WebServer.h> //Bibliothèque pour le serveur web sur ESP8266
 #include <Wire.h>
 #include <WiFiUdp.h>
 #include <NTPClient.h> //Bibliothèque pour la syncronisation de l'heure avec un serveur NTP
+#include <DHT.h> // Ajout de la bibliothèque DHT
+#include <Adafruit_Sensor.h>
 
-#define ssid      "ATD3-TP"        // SSID
-#define password  ""               // PASS
-#define DHT_PIN 2 // Le PIN GPIO2 correspond au "D4" sur le Wemos
-#define DHT_TYPE DHT11
-// #define DHT_TYPE DHT22 // Celui-ci est en commentaire car j'utiliserai un DHT11, fourni par l'enseignant
+//#define ssid      "ATD3-TP"        // SSID
+//#define password  ""               // PASS
 
-// Création objet WIFI UDP puis du client NTP
+#define ssid      "WIFI GABRIEL"     // SSID
+#define password "NuuP9mB4"          // PASS
+
+#define DHT_PIN 2 // Le PIN 2 correspond au D4 sur le Wemos
+#define DHT_TYPE DHT11 // Type du capteur (DHT11)
+// #define DHT_TYPE DHT22 // Type du capteur (DHT22)
+DHT dht(DHT_PIN, DHT_TYPE);  // Création de l'objet DHT
+
 WiFiUDP ntpUDP;
-
-// Mise à jour toutes les 60 secondes
-// Serveur NTP : europe.pool.ntp.org
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
-
-// Création des objets / create Objects
-
 ESP8266WebServer server(80);
 
-String getPage() { // CODE HTML DE LA PAGE D'ACCUEIL
+String getPage() {
   timeClient.update(); // Mise à jour de l'heure à partir du serveur NTP
   String currentTime = timeClient.getFormattedTime(); // Récupère l'heure au format "HH:MM:SS"
+  
+  // Lecture de la température du capteur DHT11
+  float temperature = dht.readTemperature();  // Lecture en Celsius
+  
+  if (isnan(temperature)) {
+    temperature = -1;  // En cas d'échec de la lecture, afficher -1
+  }
   
   String page = "<html lang=fr-FR><head><meta http-equiv='refresh' content='10'/>";
   page += "<title>Serveur Meteo Wemos</title>";
   page += "<style> body { background-color: #ffffff; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }";
   page += " #time { position: absolute; top: 10px; right: 10px; font-size: 20px; font-weight: bold; color: #0000FF; }</style>";
   page += "</head><body><h1>Wemos Weather</h1>";
-
-  // Affiche l'heure en haut à droite
+  page += "<p>Bienvenue sur le serveur Wemos Weather, qui vous permet d'obtenir des informations météorologiques</p><br>";
   page += "<div id='time'>" + currentTime + "</div>";
+  page += "<h3>La température actuelle est de : " + String(temperature) + " °C</h3>";
 
-  // Le pin DATA sur le capteur DHT11 pour le Wemos est le n°D4. Celui-ci va permettre de communiquer avec le capteur
-
-  page += "<h3>La temperature actuelle est de :</h3>";
-  page += "<br><br><p><a href='http://gabrieltordjman.fr'>- Gabriel TORDJMAN -</a></p>";
+  page += "<br><br><p><a href='https://gabrieltordjman.fr'>- Gabriel TORDJMAN -</a></p>";
   page += "</body></html>";
   
   return page;
@@ -58,28 +62,28 @@ void handleRoot() {
 void setup() {
   Serial.begin(115200);            // Initialise le moniteur série
   delay(10);
+  
   WiFi.begin(ssid, password);
   Serial.println("");
   Serial.print("Connexion au Wi-Fi en cours ...");
   
-  // Attente de la connexion au réseau WiFi 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   
-  // Affiche dans le moniteur série le SSID et l'adresse IP attribuée
   Serial.println("");
   Serial.print(ssid);
   Serial.println("  OK");
-  Serial.print("Adresse IP : ");
+  Serial.print("Votre adresse IP est : ");
   Serial.println(WiFi.localIP());
-  Serial.println("Vous êtes maintenant connecté");
-   
-  // Branche la fonction qui gère la première page 
+  Serial.println("Vous êtes maintenant connecté au Wi-Fi");
+  
+  dht.begin(); // Initialisation du capteur DHT
+  
   server.on("/", handleRoot);
   server.begin();
-  Serial.println("Serveur HTTP OK");
+  Serial.println("HTTP OK");
 }
 
 void loop() {
